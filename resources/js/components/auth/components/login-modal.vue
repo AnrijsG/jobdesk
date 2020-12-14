cf6e21bcbeb0c88fd8c12db954018a698ede2f46
Anrijs Gedroics <anrijsgedroics@gmail.com>
2020-12-14 16:12:51 +0200

    small fix

--- resources/js/components/auth/components/login-modal.vue
+++ resources/js/components/auth/components/login-modal.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="modal fade" tabindex="-1" id="loginModal" data-backdrop="false">
-            <div class="modal-dialog">
+            <div class="modal-dialog" style="z-index: 1051">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Login</h5>
