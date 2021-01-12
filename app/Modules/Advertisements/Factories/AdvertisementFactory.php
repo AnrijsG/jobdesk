079855ff07b8d126335c1820adda8ed822a40fbb
Anrijs Gedroics <anrijsgedroics@gmail.com>
2021-01-12 05:53:56 +0200

    make advertisements inactive if their expiration date is in the past

--- app/Modules/Advertisements/Factories/AdvertisementFactory.php
+++ app/Modules/Advertisements/Factories/AdvertisementFactory.php
@@ -23,14 +23,21 @@ class AdvertisementFactory
         $advertisement->location = $attributes['location'] ?? $attributes['location'] ?? 0;
         $advertisement->salary_from = $attributes['salary_from'] ?? $attributes['salaryFrom'] ?? null;
         $advertisement->salary_to = $attributes['salary_to'] ?? $attributes['salaryTo'] ?? null;
-        $advertisement->expiration_date = $attributes['expiration_date'] ?? $attributes['expirationDate'] ?? now();
-        $advertisement->is_active = $attributes['is_active'] ?? $attributes['isActive'] ?? true;
+        $advertisement->expiration_date = $attributes['expiration_date'] ?? $attributes['expirationDate'] ?? date('Y-m-d');
+        $advertisement->is_active = $this->getIsAdvertisementActive($advertisement->expiration_date);
+
         $advertisement->are_internal_applications_enabled = $attributes['are_internal_applications_enabled']
             ?? $attributes['areInternalApplicationsEnabled']
             ?? false;
+
         $advertisement->created_at = $attributes['created_at'] ?? $attributes['createdAt'] ?? now();
         $advertisement->updated_at = $attributes['updated_at'] ?? $attributes['updatedAt'] ?? now();
 
         return $advertisement;
+    }
+
+    private function getIsAdvertisementActive(string $expirationDate): bool
+    {
+        return strtotime(now()) < strtotime($expirationDate);
     }
 }
